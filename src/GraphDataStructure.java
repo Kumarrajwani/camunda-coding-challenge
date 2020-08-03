0b936c7eabec864a97b13124df5c216fe01c39be
Kumarrajwani <meshrajwani@live.com>
2020-08-04 00:14:22 +0200

    implemented Graph Data Structures

--- src/GraphDataStructure.java
+++ src/GraphDataStructure.java
@@ -3,10 +3,11 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.Map;
 
-
-public class Graph {
+//This class describes a directed graph using adjacent nodes
+public class GraphDataStructure {
     private Map<String, LinkedHashSet<String>> map = new HashMap();
 
+    //function to add edges between 2 vertices
     public void addEdge(String node1, String node2) {
         LinkedHashSet<String> adjacent = map.get(node1);
         if (adjacent == null) {
@@ -16,6 +17,7 @@ public class Graph {
         adjacent.add(node2);
     }
 
+    //Return the nodes which have an incident edge in common with node
     public LinkedList<String> adjacentNodes(String last) {
         LinkedHashSet<String> adjacent = map.get(last);
         if (adjacent == null) {
